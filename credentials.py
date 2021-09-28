37cbee2980816ffaba69845c1e044c710ad602ad
mtzfederico <fm@mtzfederico.com>
2021-09-28 18:12:08 -0500

    removed credentials.py

--- credentials.py
+++
@@ -1,9 +0,0 @@
-# WARNING: This file contains private keys
-"""
-    credentials.py
-
-    This file contains the credentials used for API's.
-    It is imported in some files.
-"""
-
-openWeatherMapsAPIKey = "API KEY GOES HERE"
