3b330fc07b80730504e8400edce7a4ca997490da
mtzfederico <fm@mtzfederico.com>
2021-09-28 18:10:19 -0500

    added credentials.py

---
+++ credentials.py
@@ -0,0 +1,9 @@
+# WARNING: This file contains private keys
+"""
+    credentials.py
+
+    This file contains the credentials used for API's.
+    It is imported in some files.
+"""
+
+openWeatherMapsAPIKey = "API KEY GOES HERE"
